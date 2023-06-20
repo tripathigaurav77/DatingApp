a115b556576e85f29948113f07aa937f22626f56
Gaurav Tripathi <tripathigaurav77@gmail.com>
2023-06-20 16:01:50 +0530

    128 add photo service, get username from claimsExtension

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 using System.Security.Claims;
 using API.DTOs;
+using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -12,11 +14,12 @@ namespace API.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UsersController(IUserRepository userRepository, IMapper mapper)
+        private readonly IPhotoService _photoService;
+        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
         {
+            _photoService = photoService;
             _mapper = mapper;
-            _userRepository = userRepository;
-            
+            _userRepository = userRepository;            
         }
 
         [HttpGet]
@@ -38,8 +41,9 @@ namespace API.Controllers
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
         {
             //Gettting username from token
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var user = await _userRepository.GetUserByUsernameAsync(username);
+            //var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            //User.GetUserName from ClaimsPrincipalExtension
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUserName());
 
             if (user == null) return NotFound();
 
@@ -48,6 +52,32 @@ namespace API.Controllers
             if (await _userRepository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Failed to update user");
+        }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUserName());
+
+            if (user == null)   return NotFound();
+
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null)   return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if (user.Photos.Count == 0) photo.IsMain = true;
+
+            user.Photos.Add(photo);
+
+            if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+
+            return BadRequest("Problem adding photo");
         }
     }
 }
---
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+using System.Security.Claims;
+
+namespace API.Extensions
+{
+  public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserName(this ClaimsPrincipal user)
+        {
+            //From Token service getting the claims
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
