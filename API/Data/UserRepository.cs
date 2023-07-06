1b0f26ff4167fb0eed8518af14b4f0b27091f182
Gaurav Tripathi <tripathigaurav77@gmail.com>
2023-07-06 16:38:38 +0530

    158 min age maxage filter API

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -33,6 +33,11 @@ namespace API.Data
             query = query.Where(u => u.UserName != userParams.CurrentUserName);
             query = query.Where(u => u.Gender == userParams.Gender);
 
+            var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+            var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
             return await PagedList<MemberDto>.CreateAsync(
                 query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider), 
                 userParams.PageNumber, 
