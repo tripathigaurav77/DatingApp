7f444a980ab475971f593c1f8e0eb89cc8e7e553
Gaurav Tripathi <tripathigaurav77@gmail.com>
2023-07-07 14:49:15 +0530

    163 enhanced action filter, updated token claims for userId

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -7,6 +7,12 @@ namespace API.Extensions
         public static string GetUserName(this ClaimsPrincipal user)
         {
             //From Token service getting the claims
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            //From Token service getting the claims
             return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
     }
