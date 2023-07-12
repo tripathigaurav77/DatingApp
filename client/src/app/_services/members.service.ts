06ef4ef1ca24f60075006993012f7ea4fdb4040a
Gaurav Tripathi <tripathigaurav77@gmail.com>
2023-07-12 20:07:37 +0530

    client likes pagination

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -95,8 +95,12 @@ export class MembersService {
     return this.http.post(this.baseUrl + 'likes/' + username, {});
   }
 
-  getLikes(predicate: string) {
-    return this.http.get<Member[]>(this.baseUrl + 'likes?predicate=' + predicate);
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('predicate', predicate);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
   }
 
   // Reuseable Pagination method
