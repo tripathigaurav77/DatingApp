209c543d4b486a6fafd744602e0f27b634c17aea
Gaurav Tripathi <tripathigaurav77@gmail.com>
2023-07-12 16:20:38 +0530

    171 added UserLike entity user defined migration

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -18,10 +18,8 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public List<Photo> Photos { get; set; } = new();
-        // public int getAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public List<Photo> Photos { get; set; } = new();        
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers { get; set; }
     }
 }
--- API/Extensions/DateTimeExtension.cs
+++ API/Extensions/DateTimeExtension.cs
@@ -5,9 +5,9 @@ namespace API.Extensions
         public static int CalculateAge(this DateOnly dob)
         {
             var today = DateOnly.FromDateTime(DateTime.UtcNow);
-            
+
             var age = today.Year - dob.Year;
-            
+
             if(dob > today.AddYears(-age)) age--;
 
             return age;
