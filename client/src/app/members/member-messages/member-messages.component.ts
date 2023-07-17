ac0cab6b33ad22ffed4f2a8acfde33e6dcdbfb81
Gaurav Tripathi <tripathigaurav77@gmail.com>
2023-07-17 14:59:04 +0530

    191 - fixing multiple loader issue for message tab

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Message } from 'src/app/_models/message';
-import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-messages',
@@ -9,19 +8,11 @@ import { MessageService } from 'src/app/_services/message.service';
 })
 export class MemberMessagesComponent implements OnInit {
   @Input() username?: string;
-  messages: Message[] = [];
+  @Input() messages: Message[] = [];
 
-  constructor(private messageService: MessageService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.loadMessages();
   }
 
-  loadMessages() {
-    if (this.username) {
-      this.messageService.getMessageThread(this.username).subscribe({
-        next: messages => this.messages = messages
-      })
-    }
-  }
 }
